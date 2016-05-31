7996bcb39b3936c223aee398258a216075cf877a
Ernie Barojas <ernie.barojas@gmail.com>
2016-05-31 14:12:38 -0500

    Initial Commit

---
+++ deprocrastinator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  deprocrastinator
+//
+//  Created by Ernie Barojas on 5/31/16.
+//  Copyright © 2016 Ernie Barojas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ deprocrastinator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  deprocrastinator
+//
+//  Created by Ernie Barojas on 5/31/16.
+//  Copyright © 2016 Ernie Barojas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
