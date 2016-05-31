01d3e113a2f07b09b892926d930f83d0f127dccc
Tyler Italiano <tyleritaliano@Tylers-MacBook-Air.local>
2016-05-31 15:35:51 -0500

    Third Commit

--- deprocrastinator/ViewController.m
+++ deprocrastinator/ViewController.m
@@ -8,15 +8,19 @@
 
 #import "ViewController.h"
 
-@interface ViewController () <UITableViewDataSource, UITableViewDelegate>
-
+@interface ViewController () <UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate>
+@property NSMutableArray *array;
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property NSMutableArray *arrayItemColor;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
+    self.array = @[@"Do dis:", @"And this:", @"Maybe this:", @"Probably this ss well:"].mutableCopy;
+    self.arrayItemColor = @[[UIColor blackColor],[UIColor greenColor],[UIColor blackColor],[UIColor redColor]].mutableCopy;
 
 
 
@@ -24,22 +28,35 @@
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     
-    return 3;
+    return self.array.count;
     
 }
 
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cellID"];
-    cell.textLabel.text = [NSString stringWithFormat:@"TEST"];
-    
-    return cell;
 
+    cell.textLabel.text = [NSString stringWithFormat:@"%@", [self.array objectAtIndex:indexPath.row]];
+    cell.textLabel.textColor = self.arrayItemColor[indexPath.row];
+    return cell;
+    
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSLog(@"did select");
+    
+    
+    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+    cell.textLabel.highlightedTextColor = [UIColor greenColor];
 
 }
 
 
 - (IBAction)addButtonPressed:(id)sender {
+    [self.array addObject:[NSString stringWithFormat:@"%@", self.textField.text]];
+    
+    [self.tableView reloadData];
+    [self.textField setText:@""];
+    [self.textField resignFirstResponder];
     
     
     
